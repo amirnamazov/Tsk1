0ebd60f8cbfd43a9063aef0ce27e43bcbef097a9
amir <amir1990@gmail.com>
2023-10-26 16:34:21 +0400

    bottom nav 100% completed

--- app/src/main/java/com/example/tsk1/MainActivity.kt
+++ app/src/main/java/com/example/tsk1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.tsk1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.tsk1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +24,15 @@ class MainActivity : AppCompatActivity() {
         this@MainActivity.title = null
         title.text = resources.getText(R.string.home_page)
         subtitle.text = resources.getText(R.string.welcome)
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.container.id)
+                as NavHostFragment
+        binding.bottomNav.setupWithNavController(navHostFragment.navController)
+
+//        setupWithNavController(binding.bottomNav, binding.container.findNavController())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
