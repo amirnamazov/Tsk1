e300a69b78f722f534ba692d55face990f4ea286
amir <amir1990@gmail.com>
2023-10-31 15:57:10 +0400

    others fragment completed

--- app/src/main/java/com/example/tsk1/others/OthersFragment.kt
+++ app/src/main/java/com/example/tsk1/others/OthersFragment.kt
@@ -1,62 +1,24 @@
 package com.example.tsk1.others
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.example.tsk1.R
+import androidx.fragment.app.viewModels
+import com.example.tsk1.base.BaseFragment
+import com.example.tsk1.databinding.FragmentOthersBinding
+import com.example.tsk1.databinding.ItemOtherBinding
+import com.example.tsk1.util.CustomAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class OthersFragment : BaseFragment<FragmentOthersBinding>(FragmentOthersBinding::inflate) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OthersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+    private val viewModel: OthersViewModel by viewModels()
 
-
-class OthersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val othersAdapter by lazy {
+        CustomAdapter(ItemOtherBinding::inflate, viewModel.listOthers.size) { i, p ->
+            i.pair = viewModel.listOthers[p]
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_others, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OthersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OthersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.root.adapter = othersAdapter
     }
 }
---
+++ app/src/main/java/com/example/tsk1/others/OthersViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.tsk1.others
+
+import androidx.lifecycle.ViewModel
+import com.example.tsk1.R
+
+class OthersViewModel : ViewModel() {
+
+    val listOthers: List<Pair<Int, String>> = listOf(
+        Pair(R.drawable.ic_warning, "Təhlükəsizlik qaydaları"),
+        Pair(R.drawable.ic_question, "Tez-tez verilən suallar"),
+        Pair(R.drawable.ic_message, "Elektron Müraciət"),
+        Pair(R.drawable.ic_location, "Regional ünvanlarımız"),
+        Pair(R.drawable.ic_headphone, "Müştəri xidmətləri"),
+        Pair(R.drawable.ic_tower, "Sərbəst güclər (info) "),
+    )
+}
