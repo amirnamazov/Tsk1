b45f35df626e557273c8da101325e765abb15d7d
amir <amir1990@gmail.com>
2023-10-31 14:16:13 +0400

    payment frag 99% completed

---
+++ app/src/main/java/com/example/tsk1/payment/PaymentViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.tsk1.payment
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+
+    val list = listOf(
+        Triple("06.03.2023", "30632 KW/s", "11.55 AZN"),
+        Triple("06.03.2023", "30632 KW/s", "11.55 AZN"),
+        Triple("06.03.2023", "3062 KW/s", "18 AZN"),
+        Triple("06.03.2023", "30632 KW/s", "155 AZN"),
+        Triple("06.03.2023", "30332 KW/s", "11.55 AZN"),
+        Triple("06.03.2023", "3062 KW/s", "115 AZN"),
+        Triple("06.03.2023", "43632 KW/s", "7.9 AZN"),
+        Triple("06.03.2023", "30632 KW/s", "11.55 AZN"),
+        Triple("06.03.2023", "30632 KW/s", "3.55 AZN"),
+        Triple("06.03.2023", "3632 KW/s", "11.85 AZN"),
+        Triple("06.03.2023", "30632 KW/s", "19.55 AZN"),
+    )
+}
